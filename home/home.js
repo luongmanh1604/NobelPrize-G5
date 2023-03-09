fe3d19d6a4886f17c4c5419af9b4bb5230147f47
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-03-09 23:42:50 +0700

    Create home.js

---
+++ home/home.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.pncchnb').click(function() {
+        $(this).parent().toggleClass('active');
+        $(this).parent().children('.pnctlnb').slideToggle();
+    });
+});
+
+$(document).ready(function() {
+    $('.pncchnb1').click(function() {
+        $(this).parent().toggleClass('active');
+        $(this).parent().children('.pnctlnb1').slideToggle();
+    });
+});
+
+$(document).ready(function() {
+    $('.pncchnb2').click(function() {
+        $(this).parent().toggleClass('active');
+        $(this).parent().children('.pnctlnb2').slideToggle();
+    });
+});
+
+$(document).ready(function() {
+    $('.pncchnb3').click(function() {
+        $(this).parent().children('.pnctlnb3').slideToggle();
+    });
+});
+$(document).ready(function() {
+    $('.pncchnb4').click(function() {
+        $(this).parent().children('.pnctlnb4').slideToggle();
+    });
+});
